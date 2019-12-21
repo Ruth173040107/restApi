20fa4f9f2ed650b2494fa6fb0a98243067dc2245
ruthsaanah <173040107@mail.unpas.ac.id>
2019-12-21 21:56:31 +0700

    menambahkan commit menampilkan search

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$('#search-input').on('click', function(){
+$('#search-button').on('click', function () {
     $.ajax({
         url: 'http://omdbapi.com',
         type: 'get',
@@ -9,7 +9,34 @@ $('#search-input').on('click', function(){
         },
 
         success: function (result) {
-            console.log(result)
+          if(result.Response == "True"){
+                let movies = result.Search;
+
+                $.each(movies, function(i, data){
+                    $('#movie-List').append(`
+                        <div class="col-md-4">
+                            <div class="card mb-3" >
+                                <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">`+ data.Title +`</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
+                                    <a href="#" class="card-link">See Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    `)
+                })
+
+
+          }else{
+              $('#movie-List').html(`
+                <div class="col">
+                    <h1 class="text-center">` + result.Error + `</h1>
+                </div>
+              
+              `)
+          }
         }
     });
 
